143d8f00ee7eb53787c1924a4ed29124798d1c56
Emmanuel Carreira Alves <emmanuel_carreira@hotmail.com>
2020-04-19 19:15:48 -0300

    faet: Add repositories routes
    1. Add middleware to check if an id is valid and a repository with this id exists.
    
    2. Fill GET, POST, PUT, DELETE routes body's.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,57 @@ app.use(cors());
 
 const repositories = [];
 
+function validId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) return response.status(400).send();
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  if (repositoryIndex == -1) return response.status(400).send();
+
+  return next();  
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories).send();
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const id = uuid();
+  const repository = { id, title, url, techs, likes: 0 };
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  const repository = repositories[repositoryIndex];
+  const newRepository = { id, title, url, techs, likes: repository.likes };
+  repositories.splice(repositoryIndex, 1, newRepository);
+
+  return response.json(newRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validId, (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validId, (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  repositories[repositoryIndex].likes += 1;
+
+  const likes = repositories[repositoryIndex].likes;
+  return response.json({ likes });
 });
 
 module.exports = app;
